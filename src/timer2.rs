e8ac0b79179fdc1faa1669759fe773b708f69eef
Marc Brinkmann <git@marcbrinkmann.de>
2017-07-12 22:16:56 +0200

    Rewrite timer2.

--- src/timer2.rs
+++ src/timer2.rs
@@ -1,174 +1,136 @@
-use std::{cmp, ops, time};
+use std::time;
 use util::NanoConv;
 
-pub trait InnerDiv {
-    fn inner_div(self, rhs: Self) -> isize;
-}
-
-impl<T> InnerDiv for T
+#[derive(Debug)]
+pub struct TimerBuilder<F, V, R>
 where
-    T: NanoConv,
+    F: Fn(time::Duration, &mut V) -> R,
 {
-    #[inline]
-    fn inner_div(self, rhs: Self) -> isize {
-        let l = self.as_ns();
-        let r = rhs.as_ns();
-
-        l.inner_div(r)
-    }
-}
-
-impl InnerDiv for u64 {
-    #[inline]
-    fn inner_div(self, rhs: Self) -> isize {
-        if self >= rhs {
-            0
-        } else {
-            (self / rhs) as isize
-        }
-    }
-}
-
-pub struct TimerBuilder<F, V, D> {
     func: F,
     initial: V,
-    interval: D,
+    interval: Option<time::Duration>,
     repeat: bool,
-    catch_up: bool,
 }
 
-impl<F, V, D> TimerBuilder<F, V, D> {
-    pub fn every(mut self, interval: D) -> Self {
-        self.interval = interval;
-        self.repeat = true;
-        self.catch_up = false;
-        self
-    }
-
-    pub fn for_every(mut self, interval: D) -> Self {
-        self.interval = interval;
-        self.repeat = true;
-        self.catch_up = true;
-        self
-    }
-
-    pub fn once_after(mut self, delay: D) -> Self {
-        self.interval = delay;
-        self.repeat = false;
-        self.catch_up = false;
-        self
-    }
-}
-
-impl<F, V, D> TimerBuilder<F, V, D> {
-    pub fn start<T>(self, now: T) -> Timer<F, V, D, T>
-    where
-        T: ops::Add<D, Output = T> + Clone,
-        D: Clone,
-    {
-        let next_tick = now.clone() + self.interval.clone();
-        Timer {
-            func: self.func,
-            value: self.initial,
-            interval: self.interval,
-            next_tick: Some(next_tick),
-            repeat: self.repeat,
-            catch_up: self.catch_up,
-            start_time: now,
-        }
-    }
-}
-
-
-pub struct Timer<F, V, D, T> {
-    func: F,
-    value: V,
-    interval: D,
-    next_tick: Option<T>,
-    repeat: bool,
-    catch_up: bool,
-    start_time: T,
-}
-
-impl<F, V, D, T> Timer<F, V, D, T>
+impl<F, V, R> TimerBuilder<F, V, R>
 where
-    D: Default,
-    F: Fn(T, &mut V),
+    F: Fn(time::Duration, &mut V) -> R,
 {
-    pub fn apply(func: F, initial: V) -> TimerBuilder<F, V, D> {
+    #[inline]
+    fn new(func: F, initial: V) -> TimerBuilder<F, V, R> {
         TimerBuilder {
             func: func,
             initial: initial,
-            interval: D::default(),
+            interval: None,
             repeat: true,
-            catch_up: false,
         }
     }
-    pub fn value(&self) -> &V {
+
+    #[inline]
+    pub fn every(mut self, interval: time::Duration) -> Self {
+        self.interval = Some(interval);
+        self.repeat = true;
+        self
+    }
+
+    #[inline]
+    pub fn once(mut self, delay: time::Duration) -> Self {
+        self.interval = Some(delay);
+        self.repeat = false;
+        self
+    }
+
+    pub fn start(self, now: time::Instant) -> Timer<F, V, R> {
+        let interval = self.interval.expect("no timing set");
+        let next_tick = now + interval;
+
+        Timer {
+            func: self.func,
+            value: self.initial,
+            interval: interval,
+            interval_ns: interval.as_ns(),
+            next_tick: next_tick,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Timer<F, V, R>
+where
+    F: Fn(time::Duration, &mut V) -> R,
+{
+    func: F,
+    value: V,
+    interval: time::Duration,
+    interval_ns: u64,
+    next_tick: time::Instant,
+}
+
+impl<F, V, R> Timer<F, V, R>
+where
+    F: Fn(time::Duration, &mut V) -> R,
+{
+    #[inline]
+    pub fn apply(func: F, initial: V) -> TimerBuilder<F, V, R> {
+        TimerBuilder::new(func, initial)
+    }
+
+    pub fn interval(&self) -> time::Duration {
+        self.interval
+    }
+
+    pub fn set_value(&mut self, value: V) {
+        self.value = value;
+    }
+
+    pub fn update(&mut self, now: time::Instant) -> Option<R> {
+        // check if timer needs to fire
+        if self.next_tick > now {
+            return None;
+        }
+
+        // calculate delta and update tick
+        let dt = now - self.next_tick + self.interval;
+
+        // calculate how many ticks we already passed
+        let dt_ns = dt.as_ns();
+        let ticks = dt_ns / self.interval_ns;
+
+        // next tick
+        self.next_tick += self.interval * ticks as u32;
+
+        // handle tick, update value
+        Some((&self.func)(dt, &mut self.value))
+    }
+}
+
+impl<F, V: Clone, R> Timer<F, V, R>
+where
+    F: Fn(time::Duration, &mut V) -> R,
+{
+    #[inline]
+    pub fn value(&self) -> V {
+        self.value.clone()
+    }
+}
+
+impl<F, V, R> AsRef<V> for Timer<F, V, R>
+where
+    F: Fn(time::Duration, &mut V) -> R,
+{
+    #[inline(always)]
+    fn as_ref(&self) -> &V {
         &self.value
     }
+}
 
-    pub fn value_mut(&mut self) -> &mut V {
+impl<F, V, R> AsMut<V> for Timer<F, V, R>
+where
+    F: Fn(time::Duration, &mut V) -> R,
+{
+    #[inline(always)]
+    fn as_mut(&mut self) -> &mut V {
         &mut self.value
-    }
-}
-
-impl<F, D, T> Timer<F, (), D, T>
-where
-    D: Default,
-    F: Fn(T),
-{
-    pub fn perform(func: F) -> TimerBuilder<F, (), D> {
-        TimerBuilder {
-            func: func,
-            initial: (),
-            interval: D::default(),
-            repeat: true,
-            catch_up: false,
-        }
-    }
-}
-
-impl<F, V, D, T> Timer<F, V, D, T>
-where
-    T: Clone
-        + ops::Sub<Output = D>
-        + cmp::PartialOrd<T>
-        + ops::Add<D, Output = T>,
-    D: Clone
-        + cmp::PartialOrd<D>
-        + ops::Sub<Output = D>
-        + InnerDiv
-        + ops::Mul<isize, Output = D>,
-    F: Fn(T, &mut V),
-{
-    pub fn update(&mut self, now: T) {
-        if let Some(ref mut next_tick) = self.next_tick {
-            // check if timer needs to fire
-            if &(*next_tick) < &now {
-                return;
-            }
-
-            // calculate delta
-            let mut dt = now.clone() - next_tick.clone();
-
-            // handle additional ticks
-            if self.catch_up {
-                while dt > self.interval {
-                    dt = dt - self.interval.clone();
-
-                    // call function and update value
-                    (self.func)(now.clone(), &mut self.value);
-                }
-            }
-
-            // handle original tick
-            (self.func)(now.clone(), &mut self.value);
-
-            // determine next tick time
-            let ticks_missed = dt.inner_div(self.interval.clone());
-            *next_tick = next_tick.clone() + self.interval.clone() * (ticks_missed + 1);
-        }
     }
 }
 
@@ -179,16 +141,84 @@ mod tests {
 
     #[test]
     fn construction() {
-        let _: Timer<_, _, _, u8> = Timer::apply(|_: u8, _| (), 123).every(3).start(0);
-        let _: Timer<_, _, _, _> = Timer::perform(|_: u8| ()).every(3).start(0);
+        let now = time::Instant::now();
+        Timer::apply(|_, _| (), 123)
+            .every(time::Duration::from_millis(500))
+            .start(now);
+        Timer::apply(
+            |_, v| {
+                *v += 1;
+                *v
+            },
+            12,
+        ).every(time::Duration::from_millis(500))
+            .start(now);
     }
 
     #[test]
     fn value_retrieval() {
-        let mut t = Timer::apply(|_, _| (), 123).every(3).start(0);
+        let now = time::Instant::now();
+        let mut t = Timer::apply(|_, _| (), 123)
+            .every(time::Duration::from_millis(500))
+            .start(now);
 
-        assert_eq!(*t.value(), 123u8);
-        assert_eq!(*t.value_mut(), 123);
+        assert_eq!(*t.as_ref(), 123);
+        assert_eq!(*t.as_mut(), 123);
+        assert_eq!(t.value(), 123);
     }
 
+    #[test]
+    fn test_single_timer() {
+        let now = time::Instant::now();
+        let mut timer = Timer::apply(|_, count| *count += 1, 0)
+            .every(time::Duration::from_millis(50))
+            .start(now);
+
+
+        assert_eq!(timer.value(), 0);
+        let future = now + time::Duration::from_millis(49);
+        timer.update(future);
+        assert_eq!(timer.value(), 0);
+        timer.update(future);
+        assert_eq!(timer.value(), 0);
+
+        let future2 = now + time::Duration::from_millis(50);
+        timer.update(future2);
+        assert_eq!(timer.value(), 1);
+        timer.update(future2);
+        assert_eq!(timer.value(), 1);
+
+        let future3 = now + time::Duration::from_millis(51);
+        timer.update(future3);
+        assert_eq!(timer.value(), 1);
+        timer.update(future3);
+        assert_eq!(timer.value(), 1);
+
+        let future4 = now + time::Duration::from_millis(100);
+        timer.update(future4);
+        assert_eq!(timer.value(), 2);
+        timer.update(future4);
+        assert_eq!(timer.value(), 2);
+
+        let future5 = now + time::Duration::from_millis(10000);
+        timer.update(future5);
+        assert_eq!(timer.value(), 3);
+        timer.update(future5);
+        assert_eq!(timer.value(), 3);
+    }
+
+    #[test]
+    fn test_just_called() {
+        let now = time::Instant::now();
+        let mut timer = Timer::apply(|_, fired| *fired = true, false)
+            .every(time::Duration::from_millis(100))
+            .start(now);
+
+        timer.update(now);
+
+        if timer.value() {
+            // reset after it fired
+            timer.set_value(false);
+        }
+    }
 }
