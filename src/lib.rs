1fd8c5324ca190725628acdd4eb3884ed2b7251d
Marc Brinkmann <git@marcbrinkmann.de>
2017-07-14 01:58:38 +0200

    Added example to top level docs.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,46 @@
 //!
 //! Contains methods for slowing down to a fixed framerate, as well as
 //! measuring actual frames per second.
+//!
+//! An example game loop:
+//!
+//! ```ignore
+//! extern crate ticktock;
+//!
+//! use std::time;
+//! use ticktock::{Clock, SecondsFloat, Timer};
+//!
+//! fn main() {
+//!     let now = time::Instant::now();
+//!
+//!     // initialize game
+//!     // ...
+//!
+//!     // show some fps measurements every 5 seconds
+//!     let mut fps_counter = Timer::apply(|delta_t, prev_tick|
+//!                                        (delta_t, *prev_tick), 0)
+//!                                 .every(time::Duration::from_secs(5))
+//!                                 .start(now);
+//!
+//!     // run with a constant framerate of 30 fps
+//!     for (tick, now) in Clock::framerate(30.0).iter() {
+//!         // this loop will run approx. every 33.3333 ms
+//!
+//!         // update, render, etc
+//!         // ...
+//!
+//!         // update or display fps count
+//!         if let Some((delta_t, prev_tick)) = fps_counter.update(now) {
+//!             fps_counter.set_value(tick);
+//!
+//!             let fps = (tick - prev_tick) as f64 / delta_t.as_fsecs();
+//!             println!("FPS: {}", fps);
+//!         }
+//!         break;  // ignore, for doctests
+//!     }
+//! }
+//! ```
+
 
 pub mod clock;
 pub mod util;
